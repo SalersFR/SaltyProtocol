ebe273c06221efb8e3502d469647609db91a9037
Salers <74661408+SalersFR@users.noreply.github.com>
2021-12-14 18:03:18 +0100

    Update WrappedInHeldItemSlot.java

--- src/main/java/eu/salers/salty/packet/wrappers/play/in/impl/WrappedInHeldItemSlot.java
+++ src/main/java/eu/salers/salty/packet/wrappers/play/in/impl/WrappedInHeldItemSlot.java
@@ -3,8 +3,6 @@ package eu.salers.salty.packet.wrappers.play.in.impl;
 import eu.salers.salty.SaltyAPI;
 import eu.salers.salty.packet.wrappers.play.in.WrappedInPacket;
 import eu.salers.salty.utils.ReflectionUtils;
-import net.minecraft.server.v1_7_R4.PacketPlayInEntityAction;
-import net.minecraft.server.v1_7_R4.PacketPlayInHeldItemSlot;
 
 public class WrappedInHeldItemSlot extends WrappedInPacket {
 
@@ -16,8 +14,6 @@ public class WrappedInHeldItemSlot extends WrappedInPacket {
     public int getSlot() {
         return get("itemInHandIndex");
     }
-
-
 
 
 }
