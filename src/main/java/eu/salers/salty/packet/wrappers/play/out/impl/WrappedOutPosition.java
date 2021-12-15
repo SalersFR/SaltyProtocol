3cd160fd00c14c3fb8e124644d702c3c2abcb24f
Salers <74661408+SalersFR@users.noreply.github.com>
2021-12-15 18:40:17 +0100

    Create WrappedOutPosition.java

---
+++ src/main/java/eu/salers/salty/packet/wrappers/play/out/impl/WrappedOutPosition.java
@@ -0,0 +1,25 @@
+package eu.salers.salty.packet.wrappers.play.out.impl;
+
+import eu.salers.salty.SaltyAPI;
+import eu.salers.salty.packet.wrappers.play.out.WrappedOutPacket;
+import eu.salers.salty.utils.ReflectionUtils;
+
+public class WrappedOutPosition extends WrappedOutPacket {
+
+    public WrappedOutPosition(Object instance, Class<?> clazz) {
+        super(instance, ReflectionUtils.getClassByPackage(SaltyAPI.get().getServerVersion().getProtocolVersionClass().
+                getPackageName(), "PacketPlayOutPosition"));
+    }
+
+    public double getX() {
+        return get("a");
+    }
+
+    public double getY() {
+        return get("b");
+    }
+
+    public double getZ() {
+        return get("c");
+    }
+}
